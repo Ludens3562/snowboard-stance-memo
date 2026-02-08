9ac93900f98a74eecda49201760b2db5efea23ea
kanon6343 <kanon6343@gmail.com>
2026-02-08 21:31:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -307,15 +307,20 @@ function render() {
   });
 }
 
-function showToast(message, time = 1600){
+function showToast(message, type = "info", time = 1600){
   const el = document.getElementById("toast");
   if (!el) return;
 
   el.textContent = message;
-  el.classList.add("show");
+
+  // 色クラス全部リセット
+  el.className = "";
+
+  // 表示 + 色
+  el.classList.add("show", type);
 
   setTimeout(() => {
-    el.classList.remove("show");
+    el.classList.remove("show", type);
   }, time);
 }
 
